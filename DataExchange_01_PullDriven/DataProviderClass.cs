4e5cb1900c5cd4ba98f53ebdbf32ebd6044d46f9
Karsten Futtrup <kaf@bila.dk>
2022-09-02 11:27:52 +0200

    Data exchange pull driven

---
+++ DataExchange_01_PullDriven/DataProviderClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataExchange_01_PullDriven
+{
+    class DataProviderClass
+    {
+        public int Value
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _value;
+                }
+            }
+        }
+        private int _value = 0;
+
+        Thread thread;
+        Random rnd;
+
+        public void StartDataProviding_SepperateThread()
+        {
+            rnd = new Random();
+            thread = new Thread(GenerateData);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void GenerateData()
+        {
+            while (true)
+            {
+                Thread.Sleep(5000);
+
+                lock (this)
+                {
+                    _value += rnd.Next(1, 10);
+                }
+            }
+        }
+    }
+}
---
+++ DataExchange_01_PullDriven/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataExchange_01_PullDriven
+{
+    class Program
+    {
+        static int localCachedValue;
+        static DataProviderClass dataProvider;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a simple app showing an implementation of pull driven data exhange");
+
+            dataProvider = new DataProviderClass();
+            dataProvider.StartDataProviding_SepperateThread();
+
+            StartPullingData();
+        }
+
+        static void StartPullingData()
+        {
+            Console.WriteLine("Star reading data every 2 seconds...");
+            int interval = 2000;            
+            while (true)
+            {
+                Thread.Sleep(interval);
+                localCachedValue = dataProvider.Value;
+                Console.WriteLine("Read value:" + localCachedValue);
+            }
+        }
+    }
+}
