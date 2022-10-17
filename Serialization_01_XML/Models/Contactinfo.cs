9d5f9c44e0502a0963b6c321a267e3d812dfdb63
Futtrup87 <fut@live.dk>
2022-10-17 19:31:34 +0200

    Added a serilization app, reading and writing xml

---
+++ Serialization_01_XML/Models/Contactinfo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_01_XML.Models
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute("contact-info", Namespace = "", IsNullable = false)]
+    public partial class contactinfo
+    {
+
+        private string nameField;
+
+        private string companyField;
+
+        private string phoneField;
+
+        /// <remarks/>
+        public string name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string company
+        {
+            get
+            {
+                return this.companyField;
+            }
+            set
+            {
+                this.companyField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string phone
+        {
+            get
+            {
+                return this.phoneField;
+            }
+            set
+            {
+                this.phoneField = value;
+            }
+        }
+    }
+
+
+}
---
+++ Serialization_01_XML/Program.cs
@@ -0,0 +1,68 @@
+﻿using Serialization_01_XML.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialization_01_XML
+{
+    internal class Program
+    {
+        static string basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This i s simple serialization test app");
+            TestWrite();
+            TestRead();
+            Console.ReadLine();
+        }
+
+        static void TestRead()
+        {
+            Console.WriteLine("\nDeSerialize test - Read file into object");
+
+            System.Xml.Serialization.XmlSerializer reader =
+                new System.Xml.Serialization.XmlSerializer(typeof(contactinfo));
+
+            string filePath = basePath + "Files\\In\\XMLFile1.xml";
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(filePath);
+
+            contactinfo xml = (contactinfo)reader.Deserialize(file);
+
+            Console.WriteLine("XML file loaded into memory done");
+        }
+
+        static void TestWrite()
+        {
+            Console.WriteLine("\nSerialize test - Create an object, serialize and create file");
+
+            contactinfo c = new contactinfo();
+            c.name = "John Doe";
+            c.phone = "+1234";
+            c.company = "Awesomeware 3000";
+
+            var serializer = new XmlSerializer(typeof(contactinfo));
+            string utf8;
+            using (StringWriter writer = new Utf8StringWriter())
+            {
+                serializer.Serialize(writer, c);
+                utf8 = writer.ToString();
+            }
+
+            string filePath = basePath + "Files\\Out\\" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".xml";
+            File.WriteAllText(filePath, utf8);
+
+            Console.WriteLine("Created XML file from object in memory done");
+        }
+    }
+
+    public class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
