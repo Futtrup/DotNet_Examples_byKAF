680a3233b15f1a49f059911554a92efd6fd06239
Karsten Futtrup <kaf@bila.dk>
2022-11-16 15:38:34 +0100

    Added Tcp Client Server serialization apps

---
+++ Client_Tcp_Serialization/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Tcp_Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Tcp Client for object serialization and send data via networkstream");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server_Tcp_Serialization/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Tcp_Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Tcp Server listening for Tcp Clients for Deserialization of objects");
+
+            Console.ReadLine();
+        }
+    }
+}
