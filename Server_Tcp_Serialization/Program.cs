d480cff44f21713ea2b410fdb99d9e04c7389f9a
Karsten Futtrup <kaf@bila.dk>
2022-11-16 15:54:27 +0100

    Initial server logic

--- Server_Tcp_Serialization/Program.cs
+++ Server_Tcp_Serialization/Program.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Server_Tcp_Serialization
 {
     class Program
     {
+        static Server srv;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Tcp Server listening for Tcp Clients for Deserialization of objects");
+
+            srv = new Server();
+            Thread srvThread = new Thread(srv.StartServer);
+            srvThread.IsBackground = false;
+            srvThread.Start();
 
             Console.ReadLine();
         }
---
+++ Server_Tcp_Serialization/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server_Tcp_Serialization
+{
+    class Server
+    {
+        TcpListener listener;
+        int port = 13000;
+
+        public Server()
+        {
+
+        }
+
+        public void StartServer()
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                Console.WriteLine("Server started...");
+
+                while (true)
+                {
+                    try
+                    {
+                        TcpClient client = listener.AcceptTcpClient();
+                        Console.WriteLine("Client connected: " + client.Client.RemoteEndPoint);
+                        ThreadPool.QueueUserWorkItem(HandleClient, client);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            Console.WriteLine("Hadling communication from client: " + client.Client.RemoteEndPoint);
+
+        }
+    }
+}
